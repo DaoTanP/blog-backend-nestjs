3ea2fcdc9c9a7ce9cb37d3cab272e315347a0ecd
Phat <daotanp@gmail.com>
2024-03-25 11:07:51 +0700

    fix variables naming, import tag and post-tag entities to module

--- src/modules/post/post.controller.spec.ts
+++ src/modules/post/post.controller.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PostController } from './post.controller';
 
 describe('PostController', () => {
-  let controller: PostController;
+  let postController: PostController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const testingModule: TestingModule = await Test.createTestingModule({
       controllers: [PostController],
     }).compile();
 
-    controller = module.get<PostController>(PostController);
+    postController = testingModule.get<PostController>(PostController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(postController).toBeDefined();
   });
 });
